332ce44c9d754fd1b3f859f7503768a6a8462830
activiteOCR <julien.terrier3@wanadoo.fr>
2023-01-27 22:02:10 +0100

    first init

---
+++ addresses.ts
@@ -0,0 +1,4 @@
+// Use your marketplace contract address here
+export const marketplaceContractAddress = '0xC9418ad4e08621b2035De2A157184aeEA12200d6';
+export const tokenContractAddress = '0x0aB71c161701a4511B9B1aE3EeFbBb2f9236dADa';
+export const stakContractAddress = '0x990CB0EFEe0352DE89400ae6ab65d2DA6B9483e8';
